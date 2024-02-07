fef24457d85d8a9ee2819e0190916e18577a2d3e
Mukund666 <mukund.choudhary14@gmail.com>
2024-02-08 01:38:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,5 +24,5 @@ values = [4500, 2500, 1053, 500]
 
     # Use `hole` to create a donut-like pie chart
 fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
-st.plotly_chart(fig)
+st.plotly_chart(fig,width=800,height=400)
 
