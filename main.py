5b1379a5a511973910d1abef2e034cdc89cf9d75
Mukund666 <mukund.choudhary14@gmail.com>
2024-02-07 16:55:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-import firebase_admin
-from firebase_admin import firestore
-
-# Application Default credentials are automatically created.
-app = firebase_admin.initialize_app()
-db = firestore.client()
-
+import streamlit as st
+from google.cloud import firestore
+ 
+# Authenticate to Firestore with the JSON account key.
+db = firestore.Client.from_service_account_json("firestore-key.json")
+ 
+# Create a reference to the Google post.
+doc_ref = db.collection("posts").document("Google")
+ 
+# Then get the data at that reference.
+doc = doc_ref.get()
+ 
+# Let's see what we got!
+st.write("The id is: ", doc.id)
+st.write("The contents are: ", doc.to_dict())
 
