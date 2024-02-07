cdab7ca19eb03e15377e5af64b086e7295ff590b
Mukund666 <mukund.choudhary14@gmail.com>
2024-02-08 01:20:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import numpy as np
 import google.cloud
 from google.cloud import firestore
 import plotly.express as px
+import plotly.graph_objects as go
+
 
 # Authenticate to Firestore with the JSON account key.
 db = firestore.Client.from_service_account_json("testdata1-20ec5-firebase-adminsdk-an9r6-d15c118c96.json")
@@ -14,6 +16,16 @@ doc_ref = db.collection("ScanData").document("5aXKu2fWBVm3OwR7rxnS")
 doc = doc_ref.get()
  
 # Let's see what we got!
-st.write("The id is: ", doc.id)
-st.write("The contents are: ", doc.to_dict())
+#st.write("The id is: ", doc.id)
+#st.write("The contents are: ", doc.to_dict())
+
+labels = ['Oxygen','Hydrogen','Carbon_Dioxide','Nitrogen']
+values = [4500, 2500, 1053, 500]
+
+    # Use `hole` to create a donut-like pie chart
+fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
+st.plotly_chart(fig, theme=None)
+
+
+
 
