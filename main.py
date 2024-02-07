c1a3a7da0574cad78738cf4e25a701f47dd33e56
Mukund666 <mukund.choudhary14@gmail.com>
2024-02-08 03:08:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -53,3 +53,27 @@ with col2:
  )
  st.plotly_chart(fig,use_container_width=True)
 
+
+chart_data = pd.DataFrame(
+   np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+   columns=['lat', 'lon'])
+
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=37.76,
+        longitude=-122.4,
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+     pdk.Layer(
+            'ScatterplotLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
+
