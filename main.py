70b571763b98cedc68009e27dc9cd8fd4a5a273a
Mukund666 <mukund.choudhary14@gmail.com>
2024-02-08 01:26:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ labels = ['Oxygen','Hydrogen','Carbon_Dioxide','Nitrogen']
 values = [4500, 2500, 1053, 500]
 
     # Use `hole` to create a donut-like pie chart
-fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3,width=800, height=400)])
-st.plotly_chart(fig, theme=None)
+fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
+st.plotly_chart(fig, theme=None,,width=800, height=400)
 
 
 
