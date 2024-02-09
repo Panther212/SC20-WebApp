f81c0ff78bd80df0ffae8608ec139caa8560072c
Mukund666 <mukund.choudhary14@gmail.com>
2024-02-09 21:05:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ new_header = df.iloc[0] #grab the first row for the header
 df = df[1:] #take the data less the header row
 df.columns = new_header #set the header row as the df header
 #Display the calendar using Streamlit components
-st.sidebar.dataframe(df,hide_index = True,width=800)
+st.sidebar._legacy_dataframe(df,hide_index = True)
 #st.write(cal_rows)
 #st.dataframe(df)
 #st.dataframe(df.style.hide(axis="index"))
