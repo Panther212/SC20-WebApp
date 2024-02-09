93cf3ab02a8803a9664cb66a022038a5b4bdc1f2
Mukund666 <mukund.choudhary14@gmail.com>
2024-02-09 23:43:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,17 +13,41 @@ st.title('Farm Analytics')
 st.markdown(
     """
 <style>
+.reportview-container .markdown-text-container {
+    font-family: monospace;
+}
+.sidebar .sidebar-content {
+    background-image: linear-gradient(#2e7bcf,#2e7bcf);
+    color: white;
+}
+.Widget>label {
+    color: white;
+    font-family: monospace;
+}
+[class^="st-b"]  {
+    color: white;
+    font-family: monospace;
+}
 .st-bb {
     background-color: transparent;
 }
 .st-at {
     background-color: #0c0080;
 }
+footer {
+    font-family: monospace;
+}
+.reportview-container .main footer, .reportview-container .main footer a {
+    color: #0c0080;
+}
+header .decoration {
+    background-image: none;
+}
+
 </style>
 """,
     unsafe_allow_html=True,
 )
-
 # Authenticate to Firestore with the JSON account key.
 db = firestore.Client.from_service_account_json("testdata1-20ec5-firebase-adminsdk-an9r6-d15c118c96.json")
  
