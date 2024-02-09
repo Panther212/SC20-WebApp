d29e861857a50848d5dd4df766253270e8d77fe9
Mukund666 <mukund.choudhary14@gmail.com>
2024-02-09 21:58:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ st.sidebar.markdown("<h2 style='text-align: center; color: white;font-size: 25px
 st.sidebar.markdown("<h2 style='text-align: center; color: #247370;font-size: 19px;'>Plot number 1 </h2>", unsafe_allow_html=True)
 st.sidebar.markdown("<h2 style='text-align: center; color: #1a5361;font-size: 19px'>Plot number 2 </h2>", unsafe_allow_html=True)
 st.sidebar.markdown("<h2 style='text-align: center; color: white;font-size: 25px;'>Pimpalgaon Farm </h2>", unsafe_allow_html=True)
-st.sidebar.markdown("<h2 style='text-align: center; color: #31458a;font-size: 19px'>Plot number 1 </h2>", unsafe_allow_html=True)
-st.sidebar.markdown("<h2 style='text-align: center; color: #738fd9;font-size: 19px'>Plot number 2 </h2>", unsafe_allow_html=True)
+st.sidebar.markdown("<h2 style='text-align: center; color: #31458a;font-size: 19px'>Plot number 3 </h2>", unsafe_allow_html=True)
+st.sidebar.markdown("<h2 style='text-align: center; color: #738fd9;font-size: 19px'>Plot number 4 </h2>", unsafe_allow_html=True)
 # Convert the calendar data into a printable format
 cal_rows = [['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']]
 cal = calendar.monthcalendar(2024, 3)
