74e42d92a935f6835e817c7bffa06936039677f2
Mukund666 <mukund.choudhary14@gmail.com>
2024-02-09 17:20:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ cal_rows = [['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']]
 for week in cal:
  cal_rows.append([str(day) if day != 0 else '' for day in week])
 # Display the calendar using Streamlit components
-st.write(cal_rows)
+st.table(cal_rows)
 
 
 col1, col2 = st.columns([2,2])
