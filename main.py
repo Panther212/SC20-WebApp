24226c2801840f659a2d8e90944076441bd21ad1
Mukund666 <mukund.choudhary14@gmail.com>
2024-02-09 21:48:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ Farmer_image = st.sidebar.image('WhatsApp Image 2024-02-09 at 21.25.12_60b97c05.
 #Farmer_name= st.sidebar.header('Ramesh Kapare')
 st.sidebar.markdown("<h1 style='text-align: center; color: white;'>Ramesh Kapare  </h1>", unsafe_allow_html=True)
 st.sidebar.markdown("<h2 style='text-align: center; color: white;'>Niphad Farm </h2>", unsafe_allow_html=True)
-st.sidebar.markdown("<h3 style='text-align: center; color: white;'>Plot 1 </h3>", unsafe_allow_html=True)
+st.sidebar.markdown("<h2 style='text-align: center; color: white;'>Plot number 1 </h2>", unsafe_allow_html=True)
 Plot_2 = st.sidebar.write('Plot 2')
 Farm_2 = st.sidebar.subheader('Pimpalgaon Farm')
 Plot_3 = st.sidebar.write('Plot 1')
