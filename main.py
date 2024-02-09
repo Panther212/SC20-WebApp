80c54b3586066ab348061de9948d27bcd43477ff
Mukund666 <mukund.choudhary14@gmail.com>
2024-02-09 20:57:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ st.sidebar.dataframe(cal_rows,hide_index = True )
 st.write(cal_rows)
 df = pd.DataFrame(cal_rows)
 df.iloc[1: , :]
-df.set_index(['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'])
+df.reindex(['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'])
 st.dataframe(df)
 st.dataframe(df.style.hide(axis="index"))
 st.markdown(df.style.hide(axis="index").to_html(), unsafe_allow_html=True)
