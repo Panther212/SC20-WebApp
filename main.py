ff4d42f4f54340a022939bfe817ec11444550099
Mukund666 <mukund.choudhary14@gmail.com>
2024-02-20 03:38:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,12 +26,17 @@ st.markdown(
 # Authenticate to Firestore with the JSON account key.
 db = firestore.Client.from_service_account_json("testdata1-20ec5-firebase-adminsdk-an9r6-d15c118c96.json")
  
-# Create a reference to the Google post.
-doc_ref = db.collection("ScanData").document("5aXKu2fWBVm3OwR7rxnS")
- 
-# Then get the data at that reference.
-doc = doc_ref.get()
- 
+# Create a reference to the all the docs 
+docs_ref = db.collection("DevMode").stream()
+i=1 
+df = pd.DataFrame()
+for doc in docs_ref:
+    TreeNos = doc.to_dict()['TreeNo']
+    result = TreeNos.items()
+    data = list(result)
+    npTrees = np.array(data)
+
+st.write(np.max(npTrees));  
 # Let's see what we got!
 #st.write("The id is: ", doc.id)
 #st.write("The contents are: ", doc.to_dict())
